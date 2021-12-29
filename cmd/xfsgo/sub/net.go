bb514a99821284f5bf5c2a85e019a51d3544fb08
younamebert <2817228206@qq.com>
2021-12-29 19:56:09 +0800

    fix delete peer about

--- cmd/xfsgo/sub/net.go
+++ cmd/xfsgo/sub/net.go
@@ -31,7 +31,7 @@ var (
 	delPeerCommand = &cobra.Command{
 		Use:                   "delpeer <node_id> [options]",
 		DisableFlagsInUseLine: true,
-		Short:                 "Add peer-to-peer link",
+		Short:                 "delete peer-to-peer link",
 		RunE:                  delPeer,
 	}
 	getNodeIdCommand = &cobra.Command{
